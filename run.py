12b7fd41f68a5b010c1863d8c74b782d4a23121c
bryan-munene <bryanmuthuri38@gmail.com>
2018-09-09 23:25:09 +0300

    [Chore #160370132] Setup the enviroment and deploy hello world code snippet for the development of the app

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 from app import app
+from flask import Flask, flash, redirect, render_template, request, session, jsonify, make_response
+import os
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ("WELCOME. You are here.")
+
+
 
 if __name__ == '__main__':
     app.run()
