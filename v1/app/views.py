1e315db6dd611df6fc193af172aba105ffb5626b
bryan-munene <bryanmuthuri38@gmail.com>
2018-09-14 20:08:51 +0300

    [feature #160368770] added the feature for a user to make an order. plus made some changes to the user section and added a travis.yml script

---
+++ v1/app/views.py
@@ -0,0 +1,259 @@
+from app import app
+from flask import Flask, request, session, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify(200,"WELCOME. You are here.")
+
+
+#USER SECTION
+users = []
+
+class Users(object):
+    @app.route("/register", methods=["POST"])
+    def register():
+        if not request.is_json:
+            return jsonify(400,"request not json")
+        else:
+            data = request.get_json() 
+            user_id =  len(users)+1
+            name = data['name']
+            email = data['email']
+            username = data['username']
+            password = data['password']
+            password2 = data['password2']
+
+        
+        if not password == password2:
+            return jsonify(403,"passwords don't match")
+
+        
+        if len(users) > 0:
+            for user in users:
+                e = user.get('email')
+                u = user.get('username')
+                p = user.get('password')
+                
+        else:
+            user = {
+                 "user_id":user_id,
+                 "name":name,
+                 "email":email,   
+                 "username":username,
+                 "password":password,
+                 "admin":False
+                 }
+
+            users.append(user)
+
+            return make_response(jsonify({"status":"created", "user":user, "users":users }),201)
+                
+                
+        if email == e:
+            return jsonify(403,"user already exists")
+                    
+        elif username == u and password == p:
+            return jsonify(403,"user already exists")
+                    
+        else:
+            user = {
+                 "user_id":user_id,
+                 "name":name,
+                 "email":email,   
+                 "username":username,
+                 "password":password,
+                 "admin":False
+                 }
+
+            users.append(user)
+
+            return make_response(jsonify({"status":"created", "user":user, "users":users }),201)
+
+
+
+    @app.route("/login")
+    def home():
+        if session.get('logged_in'):
+            return make_response(jsonify({"status":"user logged in", "id":session['user_id'], "login":True}, ),200)
+        elif session.get('logged_in_admin'):
+            return make_response(jsonify({"status":"admin logged in", "login":True}, ),200)
+        else:
+            return make_response(jsonify({"status":"login error", "login":False}),401)
+     
+    @app.route("/login", methods=["POST"])
+    def do_user_login():
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        for user in users:
+            u = user.get('username')
+            p = user.get('password')
+            r = user.get('admin')
+
+            if password == p and username == u and r == False:
+                session['user_id'] = user['user_id']
+                session['logged_in'] = True
+            elif password == p and username == u and r == True:
+                session['logged_in_admin'] = True
+            else:
+                session['logged_in'] = False
+                session['logged_in_admin'] = False
+            
+        return Users.home()
+
+    
+
+
+
+    @app.route("/logout")
+    def logout():
+        session['logged_in'] = False
+        return Users.home()
+
+
+
+#FOOD SECTION
+foods = []
+
+class Foods(object):
+    @app.route("/add_food", methods=["POST"])
+    def add_food():
+        if not session.get('logged_in'):
+            return jsonify(400,"User must be logged in")
+
+        ''' for user in users:
+        
+            user_id = user.get('user_id')
+            if not user.get('admin') and user_id != session['user_id']:
+                return jsonify(400,"User must be admin")
+ '''
+        if not request.is_json:
+            return jsonify(400,"request not json")
+        else:
+            data = request.get_json() 
+            food_id =  len(foods)+1
+            name = data['name']
+            price = data['price']
+            image = data['image']
+            createdby = session['user_id']
+
+        
+        
+
+        if len(foods) > 0:
+            for food in foods:
+                n = food.get('name')
+                p = food.get('price')
+                
+                
+        else:
+            food = {
+                 "food_id":food_id,
+                 "name":name,
+                 "price":price,   
+                 "image":image,
+                 "createdby":createdby
+                 }
+
+            foods.append(food)
+
+            return make_response(jsonify({"status":"created", "food":food, "foods":foods }),201)
+                
+                
+        if name == n and price == p:
+            return jsonify(403,"food item already exists")
+                    
+        else:
+            food = {
+                 "food_id":food_id,
+                 "name":name,
+                 "price":price,   
+                 "image":image,
+                 "createdby":createdby
+                 }
+
+            foods.append(food)
+
+            return make_response(jsonify({"status":"created", "food":food, "foods":foods }),201)
+
+
+
+#ORDERS SECTION
+orders = []
+order_items = []
+
+class Orders(object):
+    @app.route("/place_order", methods=["POST", "GET"])
+    def place_order():
+        if not session.get('logged_in'):
+            return jsonify(400,"User must be logged in")
+
+
+        if not request.is_json:
+            return jsonify(400,"request not json")
+        else:
+            data = request.get_json() 
+            order_id =  len(orders)+1
+            
+            ordered_by = session['user_id']
+            destination = data['destination']
+            payment_mode = data['payment_mode']
+            ordered_items = data['order_items']
+            
+
+            
+           
+        if destination == "" or payment_mode == "":
+            return jsonify(400,"You must fill all fields")
+        
+        else:
+            
+            for ordered_item in ordered_items:
+                food_name = ordered_item.get('food_name')
+                quantity = ordered_item.get('quantity')
+                
+                order_item_id =  len(order_items)+1
+                
+                if quantity == "":
+                    return jsonify(400,"You must fill all fields")
+
+                for food in foods:
+                    name = food.get('name')
+                    price = food.get('price')
+
+                    if food_name == name:
+                        total = int(quantity) * int(price)
+                        order_item = {
+                            "order_item_id":order_item_id,
+                            "order_id":order_id,
+                            "food_name":food_name,
+                            "Quantity":quantity,
+                            "price":price,
+                            "total":total
+                            }
+                        
+
+                        order_items.append(order_item)
+
+            order = {
+                "order_id":order_id,
+                "ordered_by":ordered_by,
+                "destination":destination,   
+                "payment_mode":payment_mode,
+                "completed_status":False,
+                "accepted_status":False
+                }
+
+            orders.append(order)           
+
+            return make_response(jsonify({"status":"created", "orders":orders, "order_items":order_items, "order":order, "order_item":order_item,}),201)
+
+    
+    
